1b48be864cc9406352ffd0ed32c4eb5d43332a76
RyderForNow <ryder@rgn.one>
2025-04-15 18:01:36 +0200

    Update

--- app.vue
+++ app.vue
@@ -5,11 +5,6 @@
   <UApp>
     <div class="h-screen bg-zinc-100">
       <NuxtPage/>
-      <footer class="bottom-2 text-center absolute bottom-0 w-screen">
-        <div class="flex justify-center">
-          <NuxtLink to="https://rgn.one/" class="hover:underline">&copy; {{new Date().getFullYear()}}, RGN.</NuxtLink>
-        </div>
-      </footer>
     </div>
   </UApp>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,29 @@
 <script setup lang="ts">
-import {MAX_META_DESCRIPTION, MAX_META_TITLE} from "~/lib/constants";
+import { MAX_META_DESCRIPTION, MAX_META_TITLE } from "~/lib/constants"
 
 const state = reactive({
   name: "Example",
   logo: "https://rgn-public.s3.rgn.one/images/brand/Logo.svg",
 
   title: "Website Title",
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut consectetur turpis. Suspendisse aliquet libero eget sodales auctor. Ut sem mauris accumsan.",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut consectetur turpis. Suspendisse aliquet libero eget sodales auctor. Ut sem mauris accumsan.",
 
   url: "https://example.com",
 })
 
-function getProgressColor(value: number, max: number): "error" | "primary" | "secondary" | "success" | "info" | "warning" | "neutral" | undefined {
+function getProgressColor(
+  value: number,
+  max: number,
+):
+  | "error"
+  | "primary"
+  | "secondary"
+  | "success"
+  | "info"
+  | "warning"
+  | "neutral"
+  | undefined {
   if (value > max) return "error"
   const delta = (max - value) / max
 
@@ -22,60 +34,103 @@ function getProgressColor(value: number, max: number): "error" | "primary" | "se
   else return "error"
 }
 
-const titleProgressColor = computed(() => getProgressColor(state.title.length, MAX_META_TITLE))
-const descriptionProgressColor = computed(() => getProgressColor(state.description.length, MAX_META_DESCRIPTION))
-
+const titleProgressColor = computed(() =>
+  getProgressColor(state.title.length, MAX_META_TITLE),
+)
+const descriptionProgressColor = computed(() =>
+  getProgressColor(state.description.length, MAX_META_DESCRIPTION),
+)
 </script>
 
 <template>
-  <div class="p-2">
-    <UForm :state="state" class="bg-zinc-200 p-4 rounded-md prose max-w-none mb-16 mx-auto w-1/2">
-      <h3 class="select-none">Settings</h3>
+  <div>
+    <div class="p-2 lg:grid grid-cols-4 min-h-screen gap-x-4">
+      <UForm
+        :state="state"
+        class="bg-zinc-200 p-4 rounded-md prose max-w-none mb-16 mx-auto max-lg:w-1/2 lg:w-full">
+        <h3 class="select-none">Settings</h3>
 
-      <h4 class="select-none">General</h4>
-      <UFormField label="Brand Name" name="name" class="mb-2">
-        <UInput v-model="state.name"/>
-      </UFormField>
-      <UFormField label="Logo" name="logo" class="mb-2">
-        <UInput v-model="state.logo"/>
-      </UFormField>
-      <UFormField label="URL" name="url" class="mb-4">
-        <UInput v-model="state.url"/>
-      </UFormField>
+        <h4 class="select-none">General</h4>
+        <UFormField label="Brand Name" name="name" class="mb-2">
+          <UInput v-model="state.name" class="w-full" />
+        </UFormField>
+        <UFormField label="Logo" name="logo" class="mb-2">
+          <UInput v-model="state.logo" class="w-full" />
+        </UFormField>
+        <UFormField label="URL" name="url" class="mb-4">
+          <UInput v-model="state.url" class="w-full" />
+        </UFormField>
 
-      <h4 class="select-none">Meta</h4>
-      <UFormField label="Title" name="title" class="mb-2">
-        <UInput v-model="state.title"/>
-        <UProgress :max="MAX_META_TITLE" :value="state.title.length" class="mt-1"
-                   :color="titleProgressColor"/>
-      </UFormField>
-      <UFormField label="Description" name="description">
-        <UTextarea v-model="state.description"/>
-        <UProgress :max="MAX_META_DESCRIPTION" :value="state.description.length" class="mt-1"
-                   :color="descriptionProgressColor"/>
-      </UFormField>
-    </UForm>
-    <div class="shadow-md bg-white p-4 flex flex-col w-[40rem] rounded-md h-33 mx-auto">
-      <div class="hover:underline hover:cursor-pointer">
-        <div class="flex flex-row items-center">
-          <Icon v-if="state.logo.length == 0" name="material-symbols:globe" class="logo mr-2 w-8 h-8 overflow-hidden rounded-full p-[2px]"/>
-          <img v-if="state.logo.length > 0" :src="state.logo" class="logo mr-2 w-8 h-8 overflow-hidden rounded-full p-[2px]"/>
-          <div class="flex flex-col overflow-hidden line-clamp-1">
-            <h3 class="text-sm">{{
-                state.name.length > 0 ? state.name : (state.url.length > 0 ? state.url : "Name")
-              }}</h3>
-            <span class="flex flex-row">
-              <h4 class="text-xs mr-1">{{ state.url.length > 0 ? state.url : "https://example.com" }}</h4>
-              <Icon name="heroicons:ellipsis-vertical"/>
-            </span>
+        <h4 class="select-none">Meta</h4>
+        <UFormField label="Title" name="title" class="mb-2">
+          <UInput v-model="state.title" class="w-full" />
+          <UProgress
+            :max="MAX_META_TITLE"
+            :model-value="state.title.length"
+            class="mt-1"
+            :color="titleProgressColor" />
+        </UFormField>
+        <UFormField label="Description" name="description">
+          <UTextarea v-model="state.description" class="w-full" />
+          <UProgress
+            :max="MAX_META_DESCRIPTION"
+            :model-value="state.description.length"
+            class="mt-1"
+            :color="descriptionProgressColor" />
+        </UFormField>
+      </UForm>
+      <div class="self-center">
+        <div
+          class="shadow-md bg-white p-4 flex flex-col w-[40rem] rounded-md h-34 mx-auto">
+          <div class="hover:underline hover:cursor-pointer">
+            <div class="flex flex-row items-center">
+              <Icon
+                v-if="state.logo.length == 0"
+                name="material-symbols:globe"
+                class="logo mr-2 w-8 h-8 overflow-hidden rounded-full p-[2px]" />
+              <img
+                v-if="state.logo.length > 0"
+                :src="state.logo"
+                class="logo mr-2 w-8 h-8 overflow-hidden rounded-full p-[2px]" />
+              <div class="flex flex-col overflow-hidden line-clamp-1">
+                <h3 class="text-sm">
+                  {{
+                    state.name.length > 0
+                      ? state.name
+                      : state.url.length > 0
+                        ? state.url
+                        : "Name"
+                  }}
+                </h3>
+                <span class="flex flex-row">
+                  <h4 class="text-xs mr-1">
+                    {{
+                      state.url.length > 0 ? state.url : "https://example.com"
+                    }}
+                  </h4>
+                  <Icon name="heroicons:ellipsis-vertical" />
+                </span>
+              </div>
+            </div>
+            <h2 class="title text-xl line-clamp-1 pb-[3px]">
+              {{ state.title.length > 0 ? state.title : "Title" }}
+            </h2>
           </div>
+          <p class="description text-sm line-clamp-2">
+            {{
+              state.description.length > 0 ? state.description : "Description"
+            }}
+          </p>
         </div>
-        <h2 class="title text-xl line-clamp-1 pb-[3px]">{{ state.title.length > 0 ? state.title : "Title" }}</h2>
       </div>
-      <p class="description text-sm line-clamp-2">{{
-          state.description.length > 0 ? state.description : "Description"
-        }}</p>
     </div>
+    <footer class="bottom-2 text-center absolute w-full">
+      <div class="flex justify-center">
+        <NuxtLink to="https://rgn.one/" class="hover:underline"
+          >&copy; {{ new Date().getFullYear() }}, RGN.
+        </NuxtLink>
+      </div>
+    </footer>
   </div>
 </template>
 
